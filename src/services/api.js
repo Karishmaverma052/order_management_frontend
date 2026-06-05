dd90a4b2f259c01a6ab51044e3adabea678a523b
Karishma <karishmaverma0532@gmail.com>
2026-06-05 12:30:30 +0530

    Url Updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://order-management-backend-b0z0.onrender.com/api';
 
 // Dashboard API
 export const getDashboardStats = async () => {
